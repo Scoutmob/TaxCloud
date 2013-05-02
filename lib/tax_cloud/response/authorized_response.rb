8bcb5729f71cfdcd14468980743e4555dd88a306
Brandon Carpenter <bhcarpenter@gmail.com>
2013-05-02 10:35:55 -0400

    Removed unnecessary initializers
    from response objects without values.

--- lib/tax_cloud/response/authorized_response.rb
+++ lib/tax_cloud/response/authorized_response.rb
@@ -1,14 +1,5 @@
 module TaxCloud
-
   class AuthorizedResponse < ResponseBase
-
-    def initialize(attrs = {})
-      attrs.each do |sym, val|
-        self.send "#{sym}=", val
-      end
-    end
-
-
   end
-
 end
+
--- lib/tax_cloud/response/authorized_with_capture_response.rb
+++ lib/tax_cloud/response/authorized_with_capture_response.rb
@@ -1,15 +1,5 @@
 module TaxCloud
-
   class AuthorizedWithCaptureResponse < ResponseBase
-
-    def initialize(attrs = {})
-      attrs.each do |sym, val|
-        self.send "#{sym}=", val
-      end
-    end
-
-    attr_accessor :authorized_with_capture_result
-
   end
-
 end
+
