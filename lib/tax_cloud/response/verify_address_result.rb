f014b5de1e08389e368fc9272447638bcfcc3cd2
Brandon Carpenter <bhcarpenter@gmail.com>
2013-05-02 10:40:15 -0400

    Made the VerifyAddressResponse consitent
    with the rest of the responses, including inheriting from ResponseBase,
    and gave it the same response codes.

--- lib/tax_cloud/response/verify_address_result.rb
+++ lib/tax_cloud/response/verify_address_result.rb
@@ -1,21 +1,35 @@
 module TaxCloud
-
-  class VerifyAddressResult
-
-    attr_accessor :address1, :address2, :city, :state, :zip5, :zip4, :err_number, :err_description
+  class VerifyAddressResult < ResponseBase
+    attr_reader :address1, :address2, :city, :state, :zip5, :zip4
 
     def initialize(attrs = {})
-       attrs.each do |sym, val|
-        self.send "#{sym}=", val
+      @address1 = attrs[:address1]
+      @address2 = attrs[:address2]
+      @city = attrs[:city]
+      @state = attrs[:state]
+      @zip5 = attrs[:zip5]
+      @zip4 = attrs[:zip4]
+
+      # Translate err_number / err_description into ResponseMessage formats
+      if '0' == attrs[:err_number]
+        attrs[:response_type] = TaxCloud::ResponseType::OK
+      else
+        attrs[:response_type] = TaxCloud::ResponseType::ERROR
+        attrs[:messages] = {
+          response_message: {
+            response_type: TaxCloud::ResponseType::ERROR,
+            message: "Error #{attrs[:err_number]}: #{attrs[:err_description]}"
+          }
+        }
       end
+
+      super
     end
 
-    def get_address
-      address = TaxCloud::Address.new(:address1 => @address1, :address2 => @address2, :city => @city, :state => @state, :zip5 => @zip5, :zip4 => @zip4)
+    def address
+      @address ||= TaxCloud::Address.new(:address1 => @address1, :address2 => @address2, :city => @city, :state => @state, :zip5 => @zip5, :zip4 => @zip4)
     end
-
 
   end
-
 end
 
